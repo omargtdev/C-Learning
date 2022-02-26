ef2cebbae56fcc812b47d31472838b72b2514dc7
Omar <omar.gutierrez.tafur@gmail.com>
2022-02-26 17:28:03 -0500

    Comments and escape sequences added

---
+++ src/comments.c
@@ -0,0 +1,25 @@
+
+int main(){
+	/*
+	 * In C, We can comment of two different ways
+	 * A line comment using //: */
+
+	// This is a line comment
+	// This is other line comment
+	
+	/* A multiline comment using / * * / like
+	 * we're using right now to explain it*/
+
+	/* 
+	 * This
+	 * is
+	 * a
+	 * multiline
+	 * comment
+	*/
+
+	/* We use comments for explain about different things
+	 * or blocks of code to others along the file */
+
+	return 0;
+}
---
+++ src/escape-sequences.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+	 /* We use escape sequences for specific use,
+	  * forgive the redundancy. For example, we
+	  * want to skip a line or create new line, we can
+	  * realize with \n */
+	 printf("This is a line\nThis is another line");
+
+	 // Or we want to use tabulator (horizontal) with \t
+	 printf("This a part of\t the sentence");
+
+	 // Or also we can escape the quotes simples or doubles
+	 printf("This is my phrase: \"Never stop learning\"\nWell isn't mine");
+
+	 /* In itself, escape sequences are bytes in format hexadecimal.
+		* Well everything here are bits and bytes so, we can represent them
+		* like 0x0A, for a new line (\n) in this case, as escape sequences.
+		*
+		* Check https://en.wikipedia.org/wiki/Escape_sequences_in_C for more info
+		* and its table. */
+
+	return 0;
+}
